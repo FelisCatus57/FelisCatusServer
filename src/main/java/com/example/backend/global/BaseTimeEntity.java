6527d3df97ab6050130b9d65fa14345319fec08c
jxxn92 <futjxxn@naver.com>
2024-03-24 23:28:42 +0900

    fix: 시간 형식 변경 년/월/일 시간-분-초 형식 설정

--- src/main/java/com/example/backend/global/BaseTimeEntity.java
+++ src/main/java/com/example/backend/global/BaseTimeEntity.java
@@ -1,14 +1,15 @@
 package com.example.backend.global;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
@@ -16,10 +17,19 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdDate;
+    private String createdDate;
 
     @LastModifiedDate
-    @Column(updatable = true)
-    private LocalDateTime lastModifiedDate;
+    private String modifiedDate;
+
+    @PrePersist
+    public void onPrePersist(){
+        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH:mm:ss"));
+        this.modifiedDate = this.createdDate;
+    }
+
+    @PreUpdate
+    public void onPreUpdate(){
+        this.modifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy년 MM월 dd일 HH:mm:ss"));
+    }
 }
