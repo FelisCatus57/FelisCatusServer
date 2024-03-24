7ef124e45050b17634742a28398ea84d17659693
jxxn92 <futjxxn@naver.com>
2024-03-24 17:04:35 +0900

    add: 게시물 응답시 유저DTO 반환 파일

---
+++ src/main/java/com/example/backend/domain/feed/dto/PostUserResponse.java
@@ -0,0 +1,35 @@
+package com.example.backend.domain.feed.dto;
+
+import com.example.backend.domain.user.entity.User;
+import com.example.backend.global.image.Image;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Schema(description = "포스트 요청시 유저 정보 응답 DTO")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostUserResponse {
+
+    @Schema(description = "유저 번호")
+    private Long id;
+
+    @Schema(description = "유저 아이디")
+    private String username;
+
+    @Schema(description = "유저 닉네임")
+    private String nickname;
+
+    @Schema(description = "유저 이미지 정보")
+    private Image image;
+
+    public PostUserResponse(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.nickname = user.getNickname();
+        this.image = user.getImage();
+    }
+}
