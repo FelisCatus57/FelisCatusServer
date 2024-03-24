0942925d16ecc171628a120e67c03842250817ba
jxxn92 <futjxxn@naver.com>
2024-03-24 15:30:16 +0900

    fix: PostService 수정 postDTO 사용 불필요 변경

--- src/main/java/com/example/backend/domain/feed/service/PostService.java
+++ src/main/java/com/example/backend/domain/feed/service/PostService.java
@@ -1,13 +1,14 @@
 package com.example.backend.domain.feed.service;
 
-import com.example.backend.domain.feed.dto.PostDTO;
 import com.example.backend.domain.feed.dto.PostUploadRequest;
+import com.example.backend.domain.feed.dto.PostUploadResponse;
 import com.example.backend.domain.feed.entity.Post;
 import com.example.backend.domain.feed.repository.PostRepository;
 import com.example.backend.domain.user.entity.User;
 import com.example.backend.global.util.AuthUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,16 +19,20 @@ public class PostService {
     private final PostRepository postRepository;
 
 
-    public void postUpload(PostUploadRequest postUploadRequest) {
+    @Transactional
+    public PostUploadResponse postUpload(PostUploadRequest postUploadRequest) {
 
+        // 현재 로그인한 유저정보를 가져온다.
         User loginUser = authUtil.getLoginUser();
 
-        PostDTO postDTO = new PostDTO(loginUser, postUploadRequest.getContent());
+//        PostDTO postDTO = new PostDTO(loginUser, postUploadRequest.getContent());
+        Post post = new Post(loginUser, postUploadRequest.getContent());
 
-        Post save = postRepository.save(postDTO.toEntity());
+        Post save = postRepository.save(post);
 
         postImageService.saveAll(save, postUploadRequest.getFiles());
 
+        return new PostUploadResponse(save.getId());
 
     }
 
