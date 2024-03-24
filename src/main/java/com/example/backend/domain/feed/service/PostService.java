609fc916bc1425ddaf954da0451a8777daa8efc0
jxxn92 <futjxxn@naver.com>
2024-03-24 23:31:45 +0900

    fix: 서비스 메소드 수정 이미지 하나만 보이는 오류 수정 -> 여러개 다 받아올 수 있도록 설정함

--- src/main/java/com/example/backend/domain/feed/service/PostService.java
+++ src/main/java/com/example/backend/domain/feed/service/PostService.java
@@ -1,14 +1,21 @@
 package com.example.backend.domain.feed.service;
 
+import com.example.backend.domain.feed.dto.PostImageResponse;
+import com.example.backend.domain.feed.dto.PostResponse;
 import com.example.backend.domain.feed.dto.PostUploadRequest;
 import com.example.backend.domain.feed.dto.PostUploadResponse;
 import com.example.backend.domain.feed.entity.Post;
+import com.example.backend.domain.feed.exception.PostNotExistedException;
 import com.example.backend.domain.feed.repository.PostRepository;
 import com.example.backend.domain.user.entity.User;
+import com.example.backend.domain.user.exception.UserNotExistedException;
 import com.example.backend.global.util.AuthUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,13 +26,13 @@ public class PostService {
     private final PostRepository postRepository;
 
 
+    // 게시물 업로드
     @Transactional
     public PostUploadResponse postUpload(PostUploadRequest postUploadRequest) {
 
         // 현재 로그인한 유저정보를 가져온다.
         User loginUser = authUtil.getLoginUser();
 
-//        PostDTO postDTO = new PostDTO(loginUser, postUploadRequest.getContent());
         Post post = new Post(loginUser, postUploadRequest.getContent());
 
         Post save = postRepository.save(post);
@@ -34,6 +41,19 @@ public class PostService {
 
         return new PostUploadResponse(save.getId());
 
+    }
+
+    // 특정 유저의 멤버 번호를 통해 해당 유저의 모든 게시물 조회
+    public List<PostResponse> getUserAllPost(Long userId) {
+
+        List<PostResponse> responses = new ArrayList<>();
+
+        for (Post post : postRepository.findAllByUserId(userId).orElseThrow(
+                () -> new PostNotExistedException()
+        )) {
+            responses.add(new PostResponse(post));
+        }
+        return responses;
     }
 
 
