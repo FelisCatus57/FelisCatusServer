89cdcd32f5497d26ad139183d7ba299ca79e5104
jxxn92 <futjxxn@naver.com>
2024-03-26 20:16:12 +0900

    fix: 댓글 작성 요청 응답 DTO

--- src/main/java/com/example/backend/domain/feed/dto/CommentUploadResponse.java
+++ src/main/java/com/example/backend/domain/feed/dto/CommentUploadResponse.java
@@ -1,16 +1,42 @@
 package com.example.backend.domain.feed.dto;
 
+import com.example.backend.domain.feed.entity.Comment;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 
-public class CommentDTO {
+public class CommentUploadResponse {
+
+    private Long commentId;
 
     private Long postId;
 
+    private Long parentId;
+
     private UserResponseDTO userResponseDTO;
 
+    private String content;
+
+    private String createdDate;
+
+    private List<CommentChildrenResponse> commentChildrenRespons;
+
+    public CommentUploadResponse(Comment comment) {
+        this.commentId = comment.getId();
+        this.postId = comment.getPost().getId();
+        if (comment.getParent() == null) {
+            this.parentId = null;
+        } else {
+            this.parentId = comment.getParent().getId();
+        }
+        this.userResponseDTO = new UserResponseDTO(comment.getUser());
+        this.content = comment.getContent();
+        this.commentChildrenRespons = new CommentChildrenResponse().CommentParentResponse(comment.getChildren());
+        this.createdDate = comment.getCreatedDate();
+    }
 
 }
