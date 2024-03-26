dff4d4778f4a1f2a7c4e39b2d7ca90a4130d67a9
jxxn92 <futjxxn@naver.com>
2024-03-27 08:57:09 +0900

    fix: DTO 이름 변경 자식 댓글 응답하는 DTO 이므로 parent -> children

--- src/main/java/com/example/backend/domain/feed/dto/CommentChildrenResponse.java
+++ src/main/java/com/example/backend/domain/feed/dto/CommentChildrenResponse.java
@@ -1,6 +1,8 @@
 package com.example.backend.domain.feed.dto;
 
 import com.example.backend.domain.feed.entity.Comment;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 
 import java.util.ArrayList;
@@ -9,16 +11,22 @@ import java.util.List;
 @Data
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@Schema(description = "댓글 요청시 자식 댓글 응답 DTO")
 public class CommentChildrenResponse {
 
+    @Schema(description = "자식 댓글 번호")
     private Long commentId;
 
+    @Schema(description = "자식 댓글 내용")
     private String content;
 
+    @Schema(description = "자식 댓글 생성날짜")
     private String createdDate;
 
+    @Schema(description = "자식 댓글 유저 정보")
     private UserResponseDTO userResponseDTO;
 
+    @Hidden
     public List<CommentChildrenResponse> CommentParentResponse(List<Comment> children) {
 
         List<CommentChildrenResponse> responses = new ArrayList<>();
