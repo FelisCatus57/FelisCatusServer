e8635e25273e989973dc52289d01a1a3a9156bc4
jxxn92 <futjxxn@naver.com>
2024-03-26 17:58:30 +0900

    add: 새로운 Exception 추가 CommentNotExistedException 댓글이 없을 시 발생하는 오류 추가

---
+++ src/main/java/com/example/backend/domain/feed/exception/CommentNotExistedException.java
@@ -0,0 +1,12 @@
+package com.example.backend.domain.feed.exception;
+
+import com.example.backend.domain.feed.entity.Comment;
+import com.example.backend.global.error.ErrorCodeMessage;
+import com.example.backend.global.error.exception.CustomException;
+
+public class CommentNotExistedException extends CustomException {
+
+    public CommentNotExistedException() {
+        super(ErrorCodeMessage.COMMENT_NOT_FOUND);
+    }
+}
