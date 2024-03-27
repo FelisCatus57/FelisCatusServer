ba24b14af7f9783599b0c93bf770957a3576f456
jxxn92 <futjxxn@naver.com>
2024-03-27 16:39:47 +0900

    del: 사용하지 않는 내용 제거 부모 댓글과 자식 댓글을 따로 생성하기 위해서 필요없는 엔티티 제거

--- src/main/java/com/example/backend/domain/feed/dto/CommentUploadRequest.java
+++ src/main/java/com/example/backend/domain/feed/dto/CommentUploadRequest.java
@@ -1,17 +1,16 @@
 package com.example.backend.domain.feed.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Schema(description = "부모 댓글 작성 요청 DTO")
 public class CommentUploadRequest {
 
-    // 댓글 부모 
-    private Long parentId;
-
-    // 댓글 내용
+    @Schema(description = "댓글 내용")
     private String content;
 
 }
