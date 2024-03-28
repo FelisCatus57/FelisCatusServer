72948b2bee0a269dd13aa71d479bc3edbe1f6496
jxxn92 <futjxxn@naver.com>
2024-03-28 12:37:07 +0900

    add: 새로운 Exception 추가 로그인한 사용자가 작성자가 아니라면 게시물을 삭제 할 수 없다는 예외추가

---
+++ src/main/java/com/example/backend/domain/feed/exception/PostCanNotDeleteException.java
@@ -0,0 +1,10 @@
+package com.example.backend.domain.feed.exception;
+
+import com.example.backend.global.error.ErrorCodeMessage;
+import com.example.backend.global.error.exception.CustomException;
+
+public class PostCanNotDeleteException extends CustomException {
+    public PostCanNotDeleteException() {
+        super(ErrorCodeMessage.POST_USER_DELETE_FAIL);
+    }
+}
