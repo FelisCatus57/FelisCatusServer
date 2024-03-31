1d1a52bc289f24139da7aae17536f2a25e4de6eb
jxxn92 <futjxxn@naver.com>
2024-03-31 20:05:20 +0900

    fix: 새로운 메소드 유저 페이지 정보 조회 메소드 위치 변경, 메소드 일부 수정

--- src/main/java/com/example/backend/domain/user/controller/UserController.java
+++ src/main/java/com/example/backend/domain/user/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.example.backend.domain.user.controller;
 
+import com.example.backend.domain.feed.service.PostService;
 import com.example.backend.domain.user.dto.UserDTO;
+import com.example.backend.domain.user.dto.UserProfileResponse;
 import com.example.backend.domain.user.dto.UserRegisterRequest;
 import com.example.backend.domain.user.entity.User;
 import com.example.backend.domain.user.service.UserService;
 import com.example.backend.global.error.ErrorResponseDTO;
 import com.example.backend.global.result.ResultCodeMessage;
 import com.example.backend.global.result.ResultResponseDTO;
+import com.example.backend.global.util.AuthUtil;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -15,17 +19,18 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@ResponseBody
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Auth", description = "유저 API")
 public class UserController {
 
+    private final AuthUtil authUtil;
     private final UserService userService;
+    private final PostService postService;
+
 
     @Operation(summary = "회원가입", description = "회원 가입")
     @ApiResponses({
@@ -33,12 +38,22 @@ public class UserController {
             @ApiResponse(responseCode = "400", description = "1. 이미 존재하는 회원일 경우 \t\n 2. 아이디가 중복 될 경우 \t\n 3. 닉네임이 중복 될 경우", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
     })
     @PostMapping("/signup")
-    @ResponseBody
     public ResponseEntity<ResultResponseDTO> signUp(@RequestBody UserRegisterRequest userRegisterRequest) throws Exception {
 
         User registerUser = userService.signUp(userRegisterRequest);
 
         return ResponseEntity.ok(ResultResponseDTO.of(ResultCodeMessage.REGISTER_SUCCESS, new UserDTO(registerUser)));
+    }
+
+    @Operation(summary = "유저 페이지", description = "유저 페이지")
+    @Parameter(name = "nickname", description = "유저 닉네임", required = true)
+    @GetMapping("/api/{nickname}")
+    public ResponseEntity<ResultResponseDTO> myPage(@PathVariable("nickname") String nickname) {
+
+        UserProfileResponse profile = userService.getProfile(nickname);
+
+        return ResponseEntity.ok(ResultResponseDTO.of(ResultCodeMessage.MY_PAGE_VIEW_SUCCESS, profile));
+
     }
 
 }
--- src/main/java/com/example/backend/domain/user/service/UserService.java
+++ src/main/java/com/example/backend/domain/user/service/UserService.java
@@ -1,28 +1,37 @@
 package com.example.backend.domain.user.service;
 
+import com.example.backend.domain.feed.dto.PostResponse;
+import com.example.backend.domain.feed.service.PostService;
 import com.example.backend.domain.user.Enum.Gender;
 import com.example.backend.domain.user.Enum.Role;
+import com.example.backend.domain.user.dto.UserProfileResponse;
 import com.example.backend.domain.user.dto.UserRegisterRequest;
 import com.example.backend.domain.user.entity.User;
 import com.example.backend.domain.user.exception.NicknameAlreadyExistedException;
+import com.example.backend.domain.user.exception.UserNotExistedException;
 import com.example.backend.domain.user.exception.UsernameAlreadyExistedException;
 import com.example.backend.domain.user.repository.UserRepository;
 import com.example.backend.global.image.Image;
 import com.example.backend.global.image.ImageType;
+import com.example.backend.global.util.AuthUtil;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
 
+    private final AuthUtil authUtil;
     private final UserRepository userRepository;
+    private final PostService postService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public User signUp(UserRegisterRequest userRegisterRequest) throws Exception{
+    public User signUp(UserRegisterRequest userRegisterRequest) {
 
         if (userRepository.existsUserByUsername(userRegisterRequest.getUsername())) {
             throw new UsernameAlreadyExistedException();
@@ -53,5 +62,26 @@ public class UserService {
         User save = userRepository.save(user);
 
         return save;
+    }
+
+    public UserProfileResponse getProfile(String nickname) {
+
+        User loginUser = authUtil.getLoginUser();
+
+        List<PostResponse> myPost = postService.getAllPostByUserNickname(nickname);
+
+        User findUser = userRepository.findByNickname(nickname).orElseThrow(
+                () -> new UserNotExistedException());
+
+        UserProfileResponse userProfileResponse = new UserProfileResponse(findUser, myPost);
+
+        if (loginUser.getId().equals(findUser.getId())) {
+            userProfileResponse.setMe(true);
+        }
+
+        userProfileResponse.setUserPostCount(postService.getUserPostCount(nickname));
+
+        return userProfileResponse;
+
     }
 }
