a6ac40acb950d62bc87cb7046db89abf4f3e11c6
jxxn92 <futjxxn@naver.com>
2024-03-31 17:44:30 +0900

    fix: 유저 컨트롤러 수정 유저 회원가입 할 경우 반환되는 정보 수정

--- src/main/java/com/example/backend/domain/user/controller/UserController.java
+++ src/main/java/com/example/backend/domain/user/controller/UserController.java
@@ -1,13 +1,17 @@
 package com.example.backend.domain.user.controller;
 
+import com.example.backend.domain.user.dto.UserDTO;
 import com.example.backend.domain.user.dto.UserRegisterRequest;
 import com.example.backend.domain.user.entity.User;
-import com.example.backend.domain.user.repository.UserRepository;
 import com.example.backend.domain.user.service.UserService;
+import com.example.backend.global.error.ErrorResponseDTO;
 import com.example.backend.global.result.ResultCodeMessage;
 import com.example.backend.global.result.ResultResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -22,20 +26,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-    private final UserRepository userRepository;
 
-    @Operation(summary = "회원가입", description = "필요 파라미터 : 아이디, 비밀번호, 이름, 닉네임, 이메일")
-    @Parameter(name = "username", description = "유저의 아이디")
-    @Parameter(name = "password", description = "유저의 비밀번호")
-    @Parameter(name = "name", description = "유저의 이름")
-    @Parameter(name = "nickname", description = "유저의 닉네임")
-    @Parameter(name = "email", description = "유저의 이메일")
+    @Operation(summary = "회원가입", description = "회원 가입")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = UserDTO.class))),
+            @ApiResponse(responseCode = "400", description = "1. 이미 존재하는 회원일 경우 \t\n 2. 아이디가 중복 될 경우 \t\n 3. 닉네임이 중복 될 경우", content = @Content(schema = @Schema(implementation = ErrorResponseDTO.class))),
+    })
     @PostMapping("/signup")
     @ResponseBody
     public ResponseEntity<ResultResponseDTO> signUp(@RequestBody UserRegisterRequest userRegisterRequest) throws Exception {
-        userService.signUp(userRegisterRequest);
-        User user = userRepository.findByUsername(userRegisterRequest.getUsername()).get();
-        return ResponseEntity.ok(ResultResponseDTO.of(ResultCodeMessage.REGISTER_SUCCESS, user));
+
+        User registerUser = userService.signUp(userRegisterRequest);
+
+        return ResponseEntity.ok(ResultResponseDTO.of(ResultCodeMessage.REGISTER_SUCCESS, new UserDTO(registerUser)));
     }
 
 }
