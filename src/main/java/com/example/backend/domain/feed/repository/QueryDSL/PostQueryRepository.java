795424b702e6b42bb914ffd5f2936df15f81ad4f
jxxn92 <futjxxn@naver.com>
2024-04-13 22:47:35 +0900

    add: QueryDsl 파일 추가

---
+++ src/main/java/com/example/backend/domain/feed/repository/QueryDSL/PostQueryRepository.java
@@ -0,0 +1,10 @@
+package com.example.backend.domain.feed.repository.QueryDSL;
+
+import com.example.backend.domain.feed.entity.Post;
+
+import java.util.List;
+
+public interface PostQueryRepository {
+
+    List<Post> findAllByFollowingPost(String nickname);
+}
---
+++ src/main/java/com/example/backend/domain/feed/repository/QueryDSL/PostQueryRepositoryImpl.java
@@ -0,0 +1,35 @@
+package com.example.backend.domain.feed.repository.QueryDSL;
+
+import com.example.backend.domain.feed.entity.Post;
+import com.example.backend.domain.feed.entity.QPost;
+import com.example.backend.domain.follow.entity.QFollow;
+import com.example.backend.domain.user.entity.QUser;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostQueryRepositoryImpl implements PostQueryRepository{
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<Post> findAllByFollowingPost(String nickname) {
+
+        QUser user = QUser.user;
+        QPost post = QPost.post;
+        QFollow follow = QFollow.follow;
+
+        List<Post> fetch = jpaQueryFactory.select(post)
+                .from(post)
+                .innerJoin(follow).on(post.user.id.eq(follow.following.id))
+                .innerJoin(user).on(user.id.eq(follow.follower.id))
+                .where(user.nickname.eq(nickname))
+                .fetch();
+
+        return fetch;
+    }
+}
