cda4d697527be6603ff4619d8df1f28dbb2444ff
jxxn92 <futjxxn@naver.com>
2024-04-13 22:51:16 +0900

    add: 좋아요 유저 목록 JPA 추가

--- src/main/java/com/example/backend/domain/feed/repository/PostLikeRepository.java
+++ src/main/java/com/example/backend/domain/feed/repository/PostLikeRepository.java
@@ -3,9 +3,13 @@ package com.example.backend.domain.feed.repository;
 import com.example.backend.domain.feed.entity.PostLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostLikeRepository extends JpaRepository<PostLike, Long> {
 
     boolean existsByUserIdAndPostId(Long userId, Long postId);
+
+    List<PostLike> findByPostId(Long postId);
 
     void deleteByUserIdAndPostId(Long userId, Long postId);
 }
