cf14c02f5a83bf26da88b229956f3bfcf7b25a75
jxxn92 <futjxxn@naver.com>
2024-04-13 22:47:53 +0900

    add: 좋아요 유저 목록 조회 응답추가

---
+++ src/main/java/com/example/backend/domain/feed/dto/PostLikeUserResponse.java
@@ -0,0 +1,34 @@
+package com.example.backend.domain.feed.dto;
+
+import com.example.backend.domain.feed.entity.PostLike;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Schema(description = "게시물 좋아요 유저 목록")
+public class PostLikeUserResponse {
+
+    @Schema(description = "좋아요 번호")
+    private Long id;
+
+    @Schema(description = "유저 번호")
+    private Long userId;
+
+    @Schema(description = "유저 닉네임")
+    private String nickname;
+
+    @Schema(description = "유저 이미지 경로")
+    private String UserProfileUrl;
+
+    public PostLikeUserResponse(PostLike postLike) {
+        this.id = postLike.getId();
+        this.userId = postLike.getUser().getId();
+        this.nickname = postLike.getUser().getNickname();
+        this.UserProfileUrl = postLike.getUser().getImage().getImageUrl();
+    }
+}
