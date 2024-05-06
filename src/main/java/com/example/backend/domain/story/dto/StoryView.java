e5a7d2a87bba35e07aa202d6644d176bd894532d
jxxn92 <futjxxn@naver.com>
2024-05-07 08:42:02 +0900

    fix: 스토리 관련 DTO 수정 DTO 설명 수정

--- src/main/java/com/example/backend/domain/story/dto/StoryView.java
+++ src/main/java/com/example/backend/domain/story/dto/StoryView.java
@@ -2,21 +2,28 @@ package com.example.backend.domain.story.dto;
 
 import com.example.backend.domain.story.entity.Story;
 import com.example.backend.domain.user.dto.MiniMenuUserResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "스토리 조회 응답 DTO")
 public class StoryView {
 
+    @Schema(description = "스토리 번호")
     private Long id;
 
+    @Schema(description = "스토리 이미지 경로")
     private String storyImageUrl;
 
+    @Schema(description = "스토리 작성 유저 정보")
     private MiniMenuUserResponse userResponse;
 
+    @Schema(description = "스토리 생성일")
     private String createdDate;
 
+    @Schema(description = "스토리 수정일")
     private String modifiedDate;
-
+    
     public StoryView(Story story) {
         this.id = story.getId();
         this.storyImageUrl = story.getImage().getImageUrl();
